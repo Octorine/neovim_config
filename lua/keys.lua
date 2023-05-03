fc92f5e0768c45491c5680e286b8b81cd2b35878
James Jackson <j.e.m.jackson@gmail.com>
2023-05-02 21:55:18 -0500

    Change set_key functions to set buffer-local keymaps instead of global ones

--- lua/keys.lua
+++ lua/keys.lua
@@ -5,12 +5,12 @@ local keys = {}
 function keys.set_key (mode, keys, actions, desc)
     local options = {noremap=true, silent=true}
     --options.desc = desc
-    vim.api.nvim_set_keymap(mode, keys, actions, options)
+    vim.api.nvim_buf_set_keymap(0, mode, keys, actions, options)
 end
 
 function keys.set_key_lua (mode, keys, lua_stuff, desc)
     local options = {noremap=true, silent=true}
     --options.desc = desc
-    vim.api.nvim_set_keymap(mode, keys, ':lua ' .. lua_stuff .. '<CR>', options)
+    vim.api.nvim_buf_set_keymap(0, mode, keys, ':lua ' .. lua_stuff .. '<CR>', options)
 end
 return keys
