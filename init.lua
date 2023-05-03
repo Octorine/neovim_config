72663690dce53d0202adf1816cbc08b8d840259a
James Jackson <j.e.m.jackson@gmail.com>
2023-05-02 22:15:19 -0500

    Use the mouse.

--- init.lua
+++ init.lua
@@ -2,11 +2,11 @@ vim.o.ts=4
 vim.o.sw=4
 vim.o.expandtab = true
 vim.o.signcolumn = 'yes'
+vim.o.mouse = 'a'
 
 vim.cmd('syntax on')
 vim.cmd('colorscheme tokyonight')
-vim.opt.formatoptions = vim.opt.formatoptions - "r" - "o";
-
+vim.opt.formatoptions = vim.opt.formatoptions - 'r' - 'o';
 
 require 'packer_setup'
 require 'lsp_setup'
