7b245094bfea9948fc6e0844fdd22dfb6edb22f3
James Jackson <j.e.m.jackson@gmail.com>
2024-06-28 12:15:14 -0500

    Change switch to terminal keybind to <leader>ct

--- lua/keys.lua
+++ lua/keys.lua
@@ -2,7 +2,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {})
 vim.keymap.set("t", "<C-g><C-g>", 
                     "<C-\\><C-n><C-w><C-w>", 
                 {desc = "Exit terminal mode and switch splits."})
-vim.keymap.set("n", "<leader>at", 
+vim.keymap.set("n", "<leader>gt", 
                     "<C-w><C-w>GA",
                 {desc = "Switch splits and enter terminal mode"})
 
